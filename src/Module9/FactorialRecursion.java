c1bf779e3814a7c99740896e26a4bfb2ede4e35d
shivadarshan gunari <shivadarshangunari@gmail.com>
2026-03-13 22:18:23 +0530

    Assignment2

---
+++ src/Module9/FactorialRecursion.java
@@ -0,0 +1,17 @@
+package Module9;
+
+public class FactorialRecursion {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial = " + factorial(n));
+    }
+}
+
+
---
+++ src/Module9/PrintNumbers.java
@@ -0,0 +1,19 @@
+package Module9;
+
+public class PrintNumbers {
+    static void printNum(int n) {
+        if (n == 0) {
+            return;
+        }
+        printNum(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNum(n);
+    }
+}
+
+
+
